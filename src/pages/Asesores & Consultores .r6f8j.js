b477baf47b347e0d6bb522d76c7db7bb0279611d
Juan Manuel Vasco Sanchez <jvascosanchez@hotmail.com>
2026-05-26 11:57:26 -0500

    Add translations for Asesores & Consultores page

--- src/pages/Asesores & Consultores .r6f8j.js
+++ src/pages/Asesores & Consultores .r6f8j.js
@@ -1,10 +1,29 @@
-// API Reference: https://www.wix.com/velo/reference/api-overview/introduction
-// “Hello, World!” Example: https://learn-code.wix.com/en/article/hello-world
+import { getCurrentLanguage } from 'public/language';
+
+const translations = {
+    en: {
+        '#text80': 'Our Specialized Team',
+        '#text81': 'Wendy Rodriguez\nExecutive Director\nCEO Financial Literacy &\nCommunity Empowerment Leader',
+        '#text83': 'Giovanny Rodriguez\nReal Estate Education, Foreclosure & Investment Specialist.',
+        '#section3title1': 'Strategic Allies & Partners'
+    },
+    es: {
+        '#text80': 'Nuestro Equipo Especializado',
+        '#text81': 'Wendy Rodriguez\nDirectora Ejecutiva\nCEO de Educación Financiera y\nLiderazgo Comunitario',
+        '#text83': 'Giovanny Rodriguez\nEspecialista en Educación de Bienes Raíces, Ejecución Hipotecaria e Inversiones.',
+        '#section3title1': 'Aliados & Socios Estratégicos'
+    }
+};
 
 $w.onReady(function () {
-    // Write your JavaScript here
-
-    // To select an element by ID use: $w('#elementID')
-
-    // Click 'Preview' to run your code
+    const lang = getCurrentLanguage();
+    applyTranslations(translations[lang]);
 });
+
+function applyTranslations(texts) {
+    Object.keys(texts).forEach(id => {
+        try {
+            $w(id).text = texts[id];
+        } catch (e) {}
+    });
+}
