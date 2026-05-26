86b6db7c1fca5ce9396ab13cc03b66084a609c73
Juan Manuel Vasco Sanchez <jvascosanchez@hotmail.com>
2026-05-26 12:03:46 -0500

    Add Spanish translations for Our Events page

--- src/pages/Our events.k32pz.js
+++ src/pages/Our events.k32pz.js
@@ -1,10 +1,24 @@
-// API Reference: https://www.wix.com/velo/reference/api-overview/introduction
-// “Hello, World!” Example: https://learn-code.wix.com/en/article/hello-world
+import { getCurrentLanguage } from 'public/language';
+
+const translations = {
+    es: {
+        '#78': 'Colecta de Juguetes 2024',
+        '#text79': 'Evento Comunitario para Adultos Mayores',
+        '#text80': 'Evento de la Iglesia'
+    }
+};
 
 $w.onReady(function () {
-    // Write your JavaScript here
-
-    // To select an element by ID use: $w('#elementID')
-
-    // Click 'Preview' to run your code
+    const lang = getCurrentLanguage();
+    if (lang === 'es') {
+        applyTranslations(translations.es);
+    }
 });
+
+function applyTranslations(texts) {
+    Object.keys(texts).forEach(id => {
+        try {
+            $w(id).text = texts[id];
+        } catch (e) {}
+    });
+}
